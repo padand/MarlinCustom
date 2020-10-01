11dfc61341632d2515027c591d2a6a34d60aee80
Andrei Paduraru <casuta.padurarului@gmail.com>
2020-10-01 20:42:03 +0300

    Working Z correction prototype, single axis

--- Marlin/zcor.cpp
+++ Marlin/zcor.cpp
@@ -1,5 +1,5 @@
 #include "zcor.h"
-#include "Marlin.h"
+#include "temperature.h"
 
 Zcor zcor; // singleton
 
@@ -7,14 +7,21 @@ Zcor zcor; // singleton
 
 void Zcor::reset(){
     SERIAL_ECHOLNPGM("Z correction reset");
+    thermalManager.babystep_Zlr(Zr_AXIS, currentCorrectionSteps[Zr_AXIS] * int(-1));
+    currentCorrectionSteps[Zr_AXIS] = 0;
 };
 void Zcor::correct(const float height){
-    SERIAL_ECHOLNPAIR("Z correction correct ", height);
-    const int c = correctionStepsZr(height);
-    SERIAL_ECHOLNPAIR("Z correction correct Zr ", c);
+    SERIAL_ECHOLNPAIR("Z correction correct at height ", height);
+    const int csZr = correctionStepsZr(height) * configured_microsteps[Z_AXIS];
+    thermalManager.babystep_Zlr(Zr_AXIS, csZr - currentCorrectionSteps[Zr_AXIS]);
+    currentCorrectionSteps[Zr_AXIS] = csZr;
 };
 
 // private:
+
+const uint8_t Zcor::configured_microsteps[] = MICROSTEP_MODES;
+
+int Zcor::currentCorrectionSteps[Zlr] = { 0 };
 
 int Zcor::correctionStepsZr(const float height) {
     const double estimatedError = ZCOR_ZR_A * sin(ZCOR_ZR_B * double(height) + ZCOR_ZR_C) + ZCOR_ZR_D;
--- Marlin/zcor.h
+++ Marlin/zcor.h
@@ -1,12 +1,16 @@
 #ifndef ZCOR_H
 #define ZCOR_H
 
+#include "MarlinConfig.h"
+
 class Zcor {
     public:
         static void reset();
         static void correct(const float height);
 
     private:
+        static int currentCorrectionSteps[Zlr];
+        static const uint8_t configured_microsteps[];
         static int correctionStepsZr(const float height);
 };
 
